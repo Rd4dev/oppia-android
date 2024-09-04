2bff617aa5477739fd03ff832906772f210e7970
Rd <rd4ramadevi@gmail.com>
2024-09-04 18:47:28 +0530

    Running with the checked in image

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -13,6 +13,7 @@ changed_files=$(git diff --name-only "$base_commit" HEAD)
 
 # Combine both lists of files, ensuring no duplicates
 all_files=$(echo -e "$staged_files\n$changed_files" | sort -u)
+echo "$all_files"
 
 function checkForBinaries() {
   binaryFilesCount=0
@@ -28,6 +29,7 @@ function checkForBinaries() {
   if [[ -n "${binaryFiles}" && "${binaryFilesCount}" -gt 0 ]]; then
     printf "\nPlease remove the following binary file(s):\n\n"
     printf "\033[33m%b\033[0m\n" "$binaryFiles"
+    printf "BINARY FILES CHECK FAILED\n"
     exit 1
   fi
 }

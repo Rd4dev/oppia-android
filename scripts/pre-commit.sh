5788d2fecdc2ef5c097164351cbe0703985c5f3b
Rd <rd4ramadevi@gmail.com>
2025-06-08 12:07:34 +0530

    Format

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-base_commit=$(git merge-base origin/develop HEAD)
-binary_files=$(git diff --numstat "$base_commit"...HEAD | awk '$1 == "-" && $2 == "-" { print $3 }')
-binary_files_count=$(echo "$binary_files" | grep -c . || true)
-
-if [[ "$binary_files_count" -gt 0 ]]; then
-  echo -e "\nDetected $binary_files_count binary file(s):"
-  echo "$binary_files"
-  echo "BINARY FILES CHECK FAILED"
-  exit 1
-else
-  echo "No binary files found in commit"
-  echo "BINARY FILES CHECK PASSED"
-fi
+bash scripts/binary_files_check.sh
+#base_commit=$(git merge-base origin/develop HEAD)
+#binary_files=$(git diff --numstat "$base_commit"...HEAD | awk '$1 == "-" && $2 == "-" { print $3 }')
+#binary_files_count=$(echo "$binary_files" | grep -c . || true)
+#
+#if [[ "$binary_files_count" -gt 0 ]]; then
+#  echo -e "\nDetected $binary_files_count binary file(s):"
+#  echo "$binary_files"
+#  echo "BINARY FILES CHECK FAILED"
+#  exit 1
+#else
+#  echo "No binary files found in commit"
+#  echo "BINARY FILES CHECK PASSED"
+#fi
 

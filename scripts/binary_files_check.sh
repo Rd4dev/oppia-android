91a9b103ad6538d2d2a2f43ad60d55b31423cd76
Rd <rd4ramadevi@gmail.com>
2025-06-08 12:25:02 +0530

    Extending existing formatting.sh resources

--- scripts/binary_files_check.sh
+++ scripts/binary_files_check.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
-# ANSI colors
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[0;33m'
-NO_COLOR='\033[0m'
+source scripts/formatting.sh
 
-base_commit=$(git merge-base origin/develop HEAD)
-binary_files=$(git diff --numstat "$base_commit"...HEAD | awk '$1 == "-" && $2 == "-" { print $3 }')
-binary_files_count=$(echo "$binary_files" | grep -c . || true)
+check_binary_files() {
+  echo "*********************************"
+  echo "Checking for Binary file presence"
+  echo "*********************************"
 
-if [[ "$binary_files_count" -gt 0 ]]; then
-  printf "%s" "Detected $binary_files_count binary file(s):"
-  printf "${YELLOW}%s\n""$binary_files"
-  printf "\n${RED}%s${NO_COLOR}\n" "BINARY FILES CHECK FAILED"
-  exit 1
-else
-  printf "${GREEN}%s${NO_COLOR}" "No binary files found in commit"
-  printf "\n${GREEN}%s${NO_COLOR}\n" "BINARY FILES CHECK PASSED"
-fi
+  base_commit=$(git merge-base origin/develop HEAD)
+  binary_files=$(git diff --numstat "$base_commit"...HEAD | awk '$1 == "-" && $2 == "-" { print $3 }')
+  binary_files_count=$(echo "$binary_files" | grep -c . || true)
+
+  if [[ "$binary_files_count" -gt 0 ]]; then
+    echo "Detected $binary_files_count binary file(s):"
+    echo "$binary_files"
+    echo_error "\nBINARY FILES CHECK FAILED"
+    exit 1
+  else
+    echo "No binary files found in commit"
+    echo_success "\nBINARY FILES CHECK PASSED"
+  fi
+}
+
+check_binary_files
--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
 
 bash scripts/binary_files_check.sh
-#base_commit=$(git merge-base origin/develop HEAD)
-#binary_files=$(git diff --numstat "$base_commit"...HEAD | awk '$1 == "-" && $2 == "-" { print $3 }')
-#binary_files_count=$(echo "$binary_files" | grep -c . || true)
-#
-#if [[ "$binary_files_count" -gt 0 ]]; then
-#  echo -e "\nDetected $binary_files_count binary file(s):"
-#  echo "$binary_files"
-#  echo "BINARY FILES CHECK FAILED"
-#  exit 1
-#else
-#  echo "No binary files found in commit"
-#  echo "BINARY FILES CHECK PASSED"
-#fi
-

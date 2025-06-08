55d26420b1842686d22fefa01a1ce568703695ab
Rd <rd4ramadevi@gmail.com>
2025-06-08 11:06:25 +0530

    Experimenting with numstat -- it does work well with staged files (but need to verify on committed files on CI)

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -1,39 +1,15 @@
 #!/bin/bash
 
-# Pre-commit hook to check for binary files.
+binary_files=$(git diff --cached --numstat | awk '$1 == "-" && $2 == "-" { print $3 }')
+binary_files_count=$(echo "$binary_files" | grep -c . || true)
 
-# Find the common ancestor between develop and the current branch
-base_commit=$(git merge-base 'origin/develop' HEAD)
+if [[ "$binary_files_count" -gt 0 ]]; then
+  echo -e "\nDetected $binary_files_count binary file(s):"
+  echo "$binary_files"
+  echo "BINARY FILES CHECK FAILED"
+  exit 1
+else
+  echo "No binary files found in commit"
+  echo "BINARY FILES CHECK PASSED"
+fi
 
-# Get the list of staged changes (files ready to be committed)
-staged_files=$(git diff --cached --name-only)
-
-# Get the list of changed files compared to the base commit
-changed_files=$(git diff --name-only "$base_commit" HEAD)
-
-# Combine both lists of files, ensuring no duplicates
-all_files=$(echo -e "$staged_files\n$changed_files" | sort -u)
-
-function checkForBinaries() {
-  binaryFilesCount=0
-  binaryFiles=()
-
-  echo "$all_files" | git check-attr --stdin binary | while IFS=: read -r filename _ status; do
-    if [[ "$status" == "set" ]]; then
-      ((binaryFilesCount++))
-      binaryFiles+=("$filename")
-      printf "\n\033[33m%s\033[0m" "$filename"
-    fi
-  done
-
-  if [[ "$binaryFilesCount" -gt 0 ]]; then
-    printf "\n\nPlease remove the %d detected binary file(s)." "$binaryFilesCount"
-    printf "\nBINARY FILES CHECK FAILED\n"
-    exit 1
-  else
-    echo "No binary files found in commit"
-    echo "BINARY FILES CHECK PASSED"
-  fi
-}
-
-checkForBinaries
